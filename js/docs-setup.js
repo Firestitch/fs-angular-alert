09a938765670dc3bbbce0d2c22c26e54a6552420
Raymond <ray@firestitch.com>
2015-12-20 12:33:32 -0500

    .

---
+++ js/docs-setup.js
@@ -0,0 +1,24 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fs-angular-alert.services:fsAlert",
+      "shortName": "fsAlert",
+      "type": "interface",
+      "moduleName": "fs-angular-alert",
+      "keywords": "add adds alert alerts amount api clear clears displaying displays error fs-angular-alert hide info interface message messages method msg optional options previous seconds services specifies success timeout type warning"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/#/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
